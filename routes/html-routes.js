d28e813f0a0fe5b83358874ad920860f0be596b1
PeeWee <oscar.oses.1127@gmail.com>
2020-09-24 19:35:14 -0400

    fixed paths in main page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,13 +4,13 @@ const path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/roam.html"));
   });
 
   app.get("/login", (req, res) => {
@@ -25,9 +25,5 @@ module.exports = function(app) {
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
-
-  app.get("/main", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
