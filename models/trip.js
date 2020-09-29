ee97d24779dbfcbfdafa2e8336af8998fcf66f95
PeeWee <oscar.oses.1127@gmail.com>
2020-09-29 12:25:02 -0400

    modified trip model to include decimals for lat/lon

--- models/trip.js
+++ models/trip.js
@@ -1,11 +1,11 @@
 module.exports = function (sequelize, DataTypes) {
     const Trip = sequelize.define("Trip", {
         lat: {
-            type: DataTypes.DECIMAL,
+            type: DataTypes.DECIMAL(11, 8),
             allowNull: false,
         },
         lon: {
-            type: DataTypes.DECIMAL,
+            type: DataTypes.DECIMAL(11, 8),
             allowNull: false
         }
     })
